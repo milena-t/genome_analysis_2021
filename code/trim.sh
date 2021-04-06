edcb8fd8086988f18191669aab1fed47b2dd1405
milena-t <milena.trabert@t-online.de>
2021-04-06 19:54:15 +0200

    trim input_output

--- code/trim.sh
+++ code/trim.sh
@@ -13,15 +13,24 @@ F=/home/milenatr/private/genome_analysis_data/rna_s1_trim/trimmed/*1P.fastq.gz
 #forward paired reads
 R=/home/milenatr/private/genome_analysis_data/rna_s1_trim/trimmed/*2P.fastq.gz
 #reverse paired reads
-echo 'Forward'
-echo ${#F[*]}
-echo 'Reverse'
-echo ${#R[*]}
+
+OUT_DIR='/home/milenatr/private/genome_analysis_2021_git/documents/trim'
 
 for i in ${!R[*]}
-do 
+do  
+    OUT_P1='$OUT_DIR${F:69:12}_P1.fastq'
+    OUT_P2='$OUT_DIR${R:69:12}_P2.fastq'
+    OUT_U1='$OUT_DIR${F:69:12}_U1.fastq'
+    OUT_U2='$OUT_DIR${R:69:12}_u2.fastq'
     echo ${F[$i]}
     echo ${R[$i]}
+    echo $OUT_P1
+    echo $OUT_P2
+    echo $OUT_U1
+    echo $OUT_U2
+    echo "\n"
+
 done
+
 
 echo 'Finished'
