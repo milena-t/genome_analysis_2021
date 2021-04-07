28aeac583e5797f2be1bcdeaa9ba5fef0f7d5704
milena-t <milena.trabert@t-online.de>
2021-04-07 20:43:19 +0200

    fastqc after trim

--- code/fastqc.sh
+++ code/fastqc.sh
@@ -8,8 +8,9 @@
 
 module load bioinfo-tools FastQC/0.11.9
 module load MultiQC/1.9
-fastqc -o /home/milenatr/private/genome_analysis_2021/documents/fastQC_first -f fastq -t 2 /home/milenatr/private/genome_analysis_data/rna_s1_trim/trimmed/*.fastq.gz
-multiqc /home/milenatr/private/genome_analysis_2021/documents/fastQC_first
+#fastqc -o /home/milenatr/private/genome_analysis_2021/documents/fastQC_first -f fastq -t 2 /home/milenatr/private/genome_analysis_data/rna_s1_trim/trimmed/*.fastq.gz
+fastqc -o /home/milenatr/private/genome_analysis_2021/documents/fastQC_after_trim -f fastq -t 2 /home/milenatr/private/genome_analysis_data/trim_by_me/*.fastq
+multiqc /home/milenatr/private/genome_analysis_2021/documents/fastQC_after_trim
 
 
 
