0a6cb662549028e35a068e4791b1b6dd076fc2d8
milena-t <milena.trabert@t-online.de>
2021-04-13 17:12:01 +0200

    assembly correct

--- code/assembly.sh
+++ code/assembly.sh
@@ -10,7 +10,7 @@ module load bioinfo-tools soapdenovo/2.04-r240
 
 OUT = '/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test'
 
-./sw/bioinfo/SOAPdenovo/2.04-r240/snowy/bin/2013 SOAPdenovo-63mer all -s soapdenovo.config \
+./sw/bioinfo/SOAPdenovo/2.04-r240/snowy/bin/SOAPdenovo-63mer all -s soapdenovo.config \
 -K 49 -p 2 -o $OUT
 
 echo 'finished assembly'
