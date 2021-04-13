9d9f9ecd5c98ed03afd5ca9f3663c6047f951d09
milena-t <milena.trabert@t-online.de>
2021-04-13 17:07:57 +0200

    assembly test

--- code/assembly.sh
+++ code/assembly.sh
@@ -6,29 +6,11 @@
 #SBATCH -t 00:20:00
 #SBATCH -J fastQC
 
-module load bioinfo-tools trimmomatic/0.36
-module load java/sun_jdk1.8.0_151
+module load bioinfo-tools soapdenovo/2.04-r240
 
-F=(/home/milenatr/private/genome_analysis_data/trim_by_me*1P.fastq.gz)
-#forward paired reads
-R=(/home/milenatr/private/genome_analysis_data/trim_by_me*2P.fastq.gz)
-#reverse paired reads
+OUT = '/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test'
 
-OUT_DIR="/home/milenatr/private/genome_analysis_data/trim_by_me"
+./sw/bioinfo/SOAPdenovo/2.04-r240/snowy/bin/2013 SOAPdenovo-63mer all -s soapdenovo.config \
+-K 49 -p 2 -o $OUT
 
-for i in ${!R[*]}
-do  
-    OUT_P1="$OUT_DIR/${F[i]:69:10}_P1.fastq"
-    OUT_P2="$OUT_DIR/${F[i]:69:10}_P2.fastq"
-    OUT_U1="$OUT_DIR/${F[i]:69:10}_U1.fastq"
-    OUT_U2="$OUT_DIR/${F[i]:69:10}_u2.fastq"
-    echo ${F[$i]}
-    echo ${R[$i]}
-
-    
-    echo 'finished assembly'
-
-done
-
-
-echo 'Finished'
+echo 'finished assembly'
