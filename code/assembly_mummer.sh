1f1a10feb3db437e247fbb16615a6a09cf2e47b4
milena-t <milena.trabert@t-online.de>
2021-04-14 15:58:54 +0200

    mummer

--- code/assembly_mummer.sh
+++ code/assembly_mummer.sh
@@ -13,9 +13,7 @@ module load bioinfo-tools MUMmer/4.0.0rc1
 # reference = /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz
 # query = /home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test.contig
 
-mummer /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz \
-/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test.contig \
-1>/home/milenatr/private/genome_analysis_2021_git/files/assembly_quality_results/mummer.out
+mummer /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz /home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test.contig 1>/home/milenatr/private/genome_analysis_2021_git/files/assembly_quality_results/mummer.out
 
 
 echo 'finished assembly'
