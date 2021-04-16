47782e98dc2f3aa86ab5220188296929aeb3b2fc
milena-t <milena.trabert@t-online.de>
2021-04-16 13:16:12 +0200

    spades

--- code/assembly_spades.sh
+++ code/assembly_spades.sh
@@ -3,14 +3,14 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 16
-#SBATCH -t 7-00:00:00
+#SBATCH -t 10:00
 #SBATCH -J assembly
  
 module load bioinfo-tools spades/3.14.1
 
 #OUT = '/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test'
 
-spades.py -rna -1 --only-assembler --dataset read_library.yaml -t 16 --tmp-dir $SNIC_TMP -k 49 \
+spades.py -rna 1 --only-assembler --dataset read_library.yaml -t 16 --tmp-dir $SNIC_TMP -k 49 \
 -o /home/milenatr/private/genome_analysis_data/spades_assembly
 
 echo 'finished assembly'
