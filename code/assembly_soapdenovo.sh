8220e4673121f5792bb52b95d29dd93d50262545
milena-t <milena.trabert@t-online.de>
2021-04-19 23:31:43 +0200

    soap assembly

--- code/assembly_soapdenovo.sh
+++ code/assembly_soapdenovo.sh
@@ -10,7 +10,7 @@ module load bioinfo-tools soapdenovo/2.04-r240
 
 #OUT = '/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test'
 
-SOAPdenovo-63mer all -s soapdenovo.config -K 49 -p 6 -o /home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test 
+SOAPdenovo-63mer all -s soapdenovo.config -K 49 -p 6 -o /home/milenatr/private/genome_analysis_data/soap_assembly 
 
 
 echo 'finished assembly'
