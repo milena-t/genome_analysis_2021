a7b730a9084776cb2c889ba905b921b9354b8744
milena-t <milena.trabert@t-online.de>
2021-04-20 14:48:47 +0200

    quast

--- code/assembly_quality_quast.sh
+++ code/assembly_quality_quast.sh
@@ -13,9 +13,10 @@ module load bioinfo-tools quast/5.0.2
 # reference = /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz
 # query = /home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test.contig
 
-python /sw/bioinfo/quast/5.0.2/snowy/bin/quast.py -o /home/milenatr/private/genome_analysis_2021_git/files/spades_assembly_quality_results \
+python /sw/bioinfo/quast/5.0.2/snowy/bin/quast.py \
+-o /home/milenatr/private/genome_analysis_2021_git/files/assembly_quality_results \
 -r /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz \
 --min-contig 50 -t 2 --eukaryote \
-/home/milenatr/private/genome_analysis_data/spades_assembly/transcripts.fasta
+/home/milenatr/private/genome_analysis_data/soap_assembly/soap_assembly.contig
 
 echo 'finished assembly'
