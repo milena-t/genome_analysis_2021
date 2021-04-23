655edf6de2faa81d5670ef89b990311dc1c1b151
milena-t <milena.trabert@t-online.de>
2021-04-23 10:21:51 +0200

    spades

--- code/assembly_spades.sh
+++ code/assembly_spades.sh
@@ -10,7 +10,7 @@ module load bioinfo-tools spades/3.14.1
 
 #OUT = '/home/milenatr/private/genome_analysis_data/soap_assembly/assembly_test'
 
-rnaspades.py --only-assembler -t 2 -m 14.9 --tmp-dir $SNIC_TMP -k 49 \
+rnaspades.py --only-assembler -t 2 -m 14 --tmp-dir $SNIC_TMP -k 49 \
 --pe1-1 /home/milenatr/private/genome_analysis_data/trim_by_me/SRR5819794_P1.fastq \
 --pe1-2 /home/milenatr/private/genome_analysis_data/trim_by_me/SRR5819794_P2.fastq \
 --pe2-1 /home/milenatr/private/genome_analysis_data/trim_by_me/SRR5819795_P1.fastq \
