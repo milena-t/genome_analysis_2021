71048d8ccdeb6a16e0cf57844b28e4006778ef7c
milena-t <milena.trabert@t-online.de>
2021-05-11 14:01:03 +0200

    braker new

--- code/braker_annotation.sh
+++ code/braker_annotation.sh
@@ -16,13 +16,33 @@ module load GenomeThreader/1.7.0
 module load samtools/1.8
 module load GeneMark/4.33-es_Perl5.24.1
 
+BAM_FILES=\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719013_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719016_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719204_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719208_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719014_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719017_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719206_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719209_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719015_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719018_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719207_Aligned.sortedByCoord.out.bam,\
+/home/milenatr/private/genome_analysis_data/mapping/results/SRR1719211_Aligned.sortedByCoord.out.bam
+
+#species directory /home/milenatr/private/genome_analysis_data/braker_annotation/species
+#bam files directory /home/milenatr/private/genome_analysis_data/mapping/results/*.bam
+
 # change directory to save output there
 cd /home/milenatr/private/genome_analysis_data/braker_annotation 
 
 braker.pl \
+--species=m.natalensis \
 --genome /home/milenatr/private/genome_analysis_data/example_scaffold/sel1_NW_015504190.fna.gz \
+--bam=$BAM_FILES \
+--softmasking
 --cores 2 \
---AUGUSTUS_CONFIG_PATH=/home/milenatr/private/genome_analysis_2021/code/augustus_config \
+--AUGUSTUS_CONFIG_PATH=/home/milenatr/private/genome_analysis_data/braker_annotation/config \
 --AUGUSTUS_BIN_PATH=/sw/bioinfo/augustus/3.4.0/snowy/bin \
 --AUGUSTUS_SCRIPTS_PATH=/sw/bioinfo/augustus/3.4.0/snowy/scripts \
 --GENEMARK_PATH=/sw/bioinfo/GeneMark/4.33-es/snowy
