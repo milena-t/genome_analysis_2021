138afdab10f7a693a2aa810226e6e9d2a516defd
milena-t <milena.trabert@t-online.de>
2021-05-18 22:28:43 +0200

    htseq for the hisat counts

--- code/htseq.sh
+++ code/htseq.sh
@@ -8,8 +8,8 @@
 
 module load bioinfo-tools htseq/0.12.4
 
-F=(/home/milenatr/private/genome_analysis_data/mapping/results/*.bam)
-OUT=/home/milenatr/private/genome_analysis_data/htseq_counting
+F=(/home/milenatr/private/genome_analysis_data/mapping/hisat_results/*)
+OUT=/home/milenatr/private/genome_analysis_data/htseq_counting/HISAT_count
 GFF=/home/milenatr/private/genome_analysis_data/gff_annotation/GCF_001595765.1_Mnat.v1_genomic.gff
 
 for i in ${!F[*]}
@@ -17,7 +17,7 @@ do
     echo '------------currently counting: '
     echo ${F[i]}
     echo 'to: '
-    echo $OUT/${F[i]:61:10}
+    echo $OUT/${F[i]:67:10}
 
     htseq-count \
     -f bam \
